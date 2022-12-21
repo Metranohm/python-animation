46f0a6d98b98a06172f1a19b3e81913296925539
Metranohm <andrewwink@gmail.com>
2022-12-21 17:08:08 -0600

    animate

--- animation.py
+++ animation.py
@@ -2,11 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Set up the figure and axes
-fig, ax = plt.subplots()
+fig, ax = plt.subplots()  # Create a figure containing a single axes. 
+ax.set_xlim((0, 10)) # Set the x-axis range
+ax.set_ylim((-1, 1)) # Set the y-axis range
+
+
 
 # Set up the data
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
+z = np.cos(x)
 
 # Set up the plot
 line, = ax.plot(x, y)
