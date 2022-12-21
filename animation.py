6abe7c553e9e1d304d01e54e05e35864a8079308
Metranohm <andrewwink@gmail.com>
2022-12-21 16:59:36 -0600

    first commit

---
+++ animation.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Set up the figure and axes
+fig, ax = plt.subplots()
+
+# Set up the data
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Set up the plot
+line, = ax.plot(x, y)
+
+# Function to update the plot
+def update(num):
+    line.set_data(x[:num], y[:num])
+    return line,
+
+# Set up the animation
+ani = FuncAnimation(fig, update, frames=range(len(x)), repeat=True)
+plt.show()
